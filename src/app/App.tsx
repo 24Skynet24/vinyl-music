ad75b82fb2779f9513c1f2fdedf6773d9da676aa
albert <albert.mnac@gmail.com>
2026-06-10 18:07:19 +0400

    feat PlayerDisk

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import PlayerControls from "../widgets/Player/ui/PlayerControls"
+import PlayerDisk from "../widgets/Player/ui/PlayerDisk"
 
 function App() {
   return (
     <>
     <main className="bg-gradient-1 w-full h-screen">
+      <PlayerDisk isPlaying/>
       <PlayerControls/>
     </main>
     </>
---
+++ src/widgets/Player/types.ts
@@ -0,0 +1,3 @@
+export interface PlayerDiskType {
+    isPlaying: boolean;
+}
