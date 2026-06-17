7cdfc38b91e77518f2c480f025d084051ab806fd
albert <albert.mnac@gmail.com>
2026-06-17 12:35:17 +0400

    feat NavigationSidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,19 @@
-import { Header } from "../widgets/Header"
+// import { Header } from "../widgets/Header"
+import { NavigationSidebar } from "../widgets/NavigationSidebar"
 import { Player } from "../widgets/Player"
 
 function App() {
   return (
     <>
     <main className="w-full pb-[32px]">
-      <Header/>
-      <div className="w-full flex items-center justify-center">
-        <Player/>
+      {/* <Header/> */}
+      <div className="w-full flex justify-between">
+        <div className="flex items-center justify-center flex-1">
+          <Player/>
+        </div>
+        <div className="my-[32px] mr-[32px]">
+          <NavigationSidebar/>
+        </div>
       </div>
     </main>
     </>
