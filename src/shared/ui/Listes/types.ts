527bcf63bb9c734edd501c20a6119fa4f73cdadb
albert <albert.mnac@gmail.com>
2026-06-17 13:13:49 +0400

    refactor PlayListItem

--- src/shared/ui/Listes/types.ts
+++ src/shared/ui/Listes/types.ts
@@ -2,5 +2,6 @@ import { TrackType } from "../../model/track"
 
 export interface PlayListItemProps extends TrackType {
     onClick: () => void
+    editPlaylist: () => void
     isSelected?: boolean
 }
