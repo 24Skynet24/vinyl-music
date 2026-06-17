3bc20b65490aca8470a9ae4b0b6bacbd9bc1d14a
albert <albert.mnac@gmail.com>
2026-06-17 16:01:26 +0400

    refactor App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,17 @@
-// import { Header } from "../widgets/Header"
-// import Blackout from "../shared/ui/Overlays/Blackout"
 import { NavigationSidebar } from "../widgets/NavigationSidebar"
 import { Player } from "../widgets/Player"
+// import { Header } from "../widgets/Header"
+// import EditPlayList from "../features/EditPlaylist/EditPlayList"
+// import Blackout from "../shared/ui/Overlays/Blackout"
+// import { SlidingPanel } from "../widgets/SlidingPanel"
 
 function App() {
   return (
     <>
     <main className="w-full pb-[32px]">
+      {/* <EditPlayList/> */}
       {/* <Blackout/> */}
+      {/* <SlidingPanel/> */}
       {/* <Header/> */}
       <div className="w-full flex justify-between">
         <div className="flex items-center justify-center flex-1">
