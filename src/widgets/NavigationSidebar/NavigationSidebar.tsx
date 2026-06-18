0ae612943a978461629282856fe4f98827a273ed
Albert <ksk836684@gmail.com>
2026-06-18 15:32:46 +0400

    style NavSideBar sizes

--- src/widgets/NavigationSidebar/NavigationSidebar.tsx
+++ src/widgets/NavigationSidebar/NavigationSidebar.tsx
@@ -2,7 +2,7 @@ import NavSidebarButton from "../../shared/ui/Buttons/NavSidebarButton"
 
 function NavigationSidebar () {
     return (
-        <aside className="w-[128px] h-[800px] py-[32px] bg-black-main/30 flex flex-col gap-[32px] items-center rounded-[16px]">
+        <aside className="h-[800px] p-[16px] bg-black-main/30 flex flex-col gap-[32px] items-center rounded-[16px]">
             <NavSidebarButton
                 onClick={() => {}}
                 isPlaying={true}
